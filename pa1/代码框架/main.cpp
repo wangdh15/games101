c06b63b01c02803cc0b99767cfad6303793e08cc
wangdh15 <13020023780@163.com>
2022-05-07 14:31:38 +0800

    finish pa1

--- pa1/代码框架/main.cpp
+++ pa1/代码框架/main.cpp
@@ -6,6 +6,7 @@
 
 constexpr double MY_PI = 3.1415926;
 
+// move the object to origin.
 Eigen::Matrix4f get_view_matrix(Eigen::Vector3f eye_pos)
 {
     Eigen::Matrix4f view = Eigen::Matrix4f::Identity();
@@ -19,6 +20,7 @@ Eigen::Matrix4f get_view_matrix(Eigen::Vector3f eye_pos)
     return view;
 }
 
+// 该函数的作用是，将物体沿着z轴旋转rotation_angle度数（角度）
 Eigen::Matrix4f get_model_matrix(float rotation_angle)
 {
     Eigen::Matrix4f model = Eigen::Matrix4f::Identity();
@@ -27,9 +29,20 @@ Eigen::Matrix4f get_model_matrix(float rotation_angle)
     // Create the model matrix for rotating the triangle around the Z axis.
     // Then return it.
 
+    Eigen::Matrix4f translate;
+    float angle_pi = rotation_angle / 180.f * MY_PI;
+
+    translate << std::cos(angle_pi), -std::sin(angle_pi), 0, 0,
+                 std::sin(angle_pi), std::cos(angle_pi), 0, 0,
+                 0, 0, 1, 0,
+                 0, 0, 0, 1;
+    model = translate * model;
+
     return model;
 }
 
+// 获取project矩阵
+// eye_fov为角度
 Eigen::Matrix4f get_projection_matrix(float eye_fov, float aspect_ratio,
                                       float zNear, float zFar)
 {
@@ -40,8 +53,48 @@ Eigen::Matrix4f get_projection_matrix(float eye_fov, float aspect_ratio,
     // TODO: Implement this function
     // Create the projection matrix for the given parameters.
     // Then return it.
+    Eigen::Matrix4f M_ortho, M_persp_ortho;
+    // first do persp->ortho
+    float eye_fov_pi = eye_fov / 180.f * MY_PI;
+    float t = fabs(zNear) * std::tan(eye_fov_pi / 2);
+    float r = aspect_ratio * t;
+    float b = -t, l = -r;
+    float n = -std::fabs(zNear), f = -std::fabs(zFar);
+
+    M_persp_ortho  << -std::fabs(zNear), 0, 0, 0,
+               0, -std::fabs(zNear), 0, 0,
+               0, 0, -std::fabs(zNear) - std::fabs(zFar), -std::fabs(zNear * zFar),
+               0, 0, 1, 0;
+
+    M_ortho << 2.0 / (r - l), 0, 0, 0,
+                     0, 2.0 / (t - b), 0, 0,
+                     0, 0, 2.0 / (n - f), 0,
+                     0, 0, 0, 1;
+
+    projection = M_ortho * M_persp_ortho * projection;
 
     return projection;
+}
+
+
+// 绕axis方向的轴旋转angle角度
+Eigen::Matrix4f get_rotation(Vector3f axis, float angle) {
+    float angle_pi = angle / 180 * MY_PI;
+    Eigen::Matrix3f tmp1, tmp2, tmp3, tmp4;
+    tmp1 = std::cos(angle_pi) * Eigen::Matrix3f::Identity();
+    tmp2 = (1 - std::cos(angle_pi)) * axis * axis.transpose();
+    tmp4 << 0, -axis[2], axis[1],
+            axis[2], 0, -axis[0],
+            -axis[1], axis[0], 0;
+    tmp3 = std::sin(angle_pi) * tmp4;
+    Eigen::Matrix3f ret3d = tmp1 + tmp2 + tmp3;
+    Eigen::Matrix4f ret = Eigen::Matrix4f::Identity();
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            ret(i,j) = ret3d(i, j);
+        }
+    }
+    return ret;
 }
 
 int main(int argc, const char** argv)
@@ -77,7 +130,8 @@ int main(int argc, const char** argv)
     if (command_line) {
         r.clear(rst::Buffers::Color | rst::Buffers::Depth);
 
-        r.set_model(get_model_matrix(angle));
+        // r.set_model(get_model_matrix(angle));
+        r.set_model(get_rotation({0, 0, 1}, angle));
         r.set_view(get_view_matrix(eye_pos));
         r.set_projection(get_projection_matrix(45, 1, 0.1, 50));
 
